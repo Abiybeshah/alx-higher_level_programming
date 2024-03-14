cba74f41e6bf13aab9c122a6ba304b5b089ac5bb
Abiybeshah <abiyjem99@gmail.com>
2024-03-15 02:42:38 +0300

    variable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    i = len(argv) - 1
+    import sys
+    i = len(argv.argv) - 1
     if i == 0:
         print("{} arguments.".format(i))
     elif i == 1:
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     if i >= 1:
         i = 0
-        for arg in argv:
+        for arg in sys.argv:
             if i != 0:
                 print("{}: {}".format(i, arg))
-	i += 1
+	    i += 1

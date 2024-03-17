5009215d5c86ee386ce051db314dd91649b6a33a
Abiybeshah <abiyjem99@gmail.com>
2024-03-18 02:59:58 +0300

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in mylist:
+    for i in my_list:
         print("{:d}".format(mylist[i - 1]))
         if len(my_list) == i:
             print("{:d}".format(my_list(i), end=""))

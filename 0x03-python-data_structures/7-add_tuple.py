261b8d695749d72936a58475664e54da6bc7e802
Abiybeshah <abiyjem99@gmail.com>
2024-03-19 01:16:35 +0300

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a = (), tuple_b = ()):
+def add_tuple(tuple_a=(), tuple_b=()):
     tone = resolve(tuple_a)
     ttwo = resolve(tuple_b)
     return (tone[0] + ttwo[0], tone[1] + ttwo[1])
 
-def resolve(t = ()):
+def resolve(t=()):
     t_len = len(t)
     if t_len == 0:
         t_new = 0, 0

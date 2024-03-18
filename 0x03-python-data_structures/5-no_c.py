9308ef3c498b6bdb342502d3cafe62913f0e3f5e
Abiybeshah <abiyjem99@gmail.com>
2024-03-19 00:33:29 +0300

    no

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bim/python3
+def no_c(my_string):
+    listchar = list(my_string)
+        for char in listchar:
+            if char == 'c' or char == 'C':
+                listchar.remove(char)
+                return ("".join(listchar))

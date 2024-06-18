ab37a79b993600cb8780ac9574bae24eb79fe86b
Abiybeshah <abiyjem99@gmail.com>
2024-06-18 23:09:56 +0300

    arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+/* arguments on javascript */
+const args = process.argv;
+if (args[2] <= undefined) {
+	console.log('No argument');
+}
+else if (args.length === 3)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Arguments found');
+}

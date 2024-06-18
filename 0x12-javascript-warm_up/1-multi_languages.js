a7c6dd649fff922f3ada6879bfa3c076c46ea0fd
Abiybeshah <abiyjem99@gmail.com>
2024-06-18 22:47:48 +0300

    multi-language

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+/* a szript that prints 3 ;ines */
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'JavaScript is amazing';
+console.log(first'\n', second'\n', third'\n');

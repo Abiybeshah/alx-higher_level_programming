355302f87b1fee567973e899e1dd45e01f923a7e
Abiybeshah <abiyjem99@gmail.com>
2024-06-20 22:36:32 +0300

    to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,13 +2,11 @@
 /* write a script that prints my number: first arg converted
 * in to integers
 */
-
 const args = process.argv[2];
 
 if (args === undefined || isNaN(parseInt(args))) {
   console.log('Not a number');
 
-}
-else {
+} else {
   console.log('My number: ' + parseInt(args));
 }

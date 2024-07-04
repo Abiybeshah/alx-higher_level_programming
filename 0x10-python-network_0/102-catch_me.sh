d378b4a56969e1d2541a9a0bb7e1ffa45c69ec61
Abiybeshah <abiyjem99@gmail.com>
2024-07-05 02:30:51 +0300

    python networking

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # a Bash script that makes a request to 0.0.0.0:5000/catch_m
-#ethat causes the server to respond
-curl -sL -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
+# that causes the server to respond
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"

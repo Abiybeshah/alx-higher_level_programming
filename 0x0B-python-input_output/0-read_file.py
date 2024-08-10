748d5bf695fb708bd7831826903515e13f1a58ea
Abiybeshah <abiyjem99@gmail.com>
2024-08-10 11:47:07 +0300

    python import

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+# read file
 
 def read_file(filename=""):
-    with open(filename, encoding='UTF-8') as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
